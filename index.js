d178708afb1694d45050f388c626d7c905f6c33b
Mohammed3927 <gkhal839@gmail.com>
2025-01-16 10:56:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening To ASWAYZ Community","Streaming on kick","kick.com/mtnews];
+const statusMessages = ["Listening To ASWAYZ Community","Streaming on kick","kick.com/mtnews"];
 
 
 let currentIndex = 0;
